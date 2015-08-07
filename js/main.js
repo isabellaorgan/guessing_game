1a1664b089944ed21148ecb5cd770a082c61237c
Isabella Organ <isabellaorgan@gmail.com>
2015-08-07 13:42:55 -0700

    add img and js dirs, change game to index, clean up some img src and other html code, begin dom manipulation

---
+++ js/main.js
@@ -0,0 +1,31 @@
+  <script>
+  	var guess, answer, message, userAttempts;
+  	// answer is actual number of records I own
+  	answer = 380;
+  	userAttempts = 0;
+  	message = "Don't worry about it, how could you have known?";
+  	console.log(answer);
+  	while (userAttempts < 5 && guess != answer){
+  		guess = prompt("How many vinyl records do I own?");
+  		if (guess == answer) {
+  			message = "You must be a stalker!";
+  		}
+  			// did not properly terminate lines 11 and 13 in original code
+  			// fixed next day
+  		userAttempts++;
+  	}
+
+  	   var recordCounter = document.getElementById('record-counter');
+    recordCounter.innerHTML = message;
+
+fumction = addToHintList() {
+  var hintListItem = document.createElement('li';)
+  var hintNode = document.createTextNode(hint1);
+  hintListItem.appendChild(hintNode);
+  document.getElementById('hint-list').appendChild(hintListItem);
+  }
+  	// alert(message);
+  	console.log(userAttempts);
+  </script>
+
+
