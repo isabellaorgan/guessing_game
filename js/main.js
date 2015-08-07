95513dd36b29992fa6bfa8592a0f44b6db2a9be7
Isabella Organ <isabellaorgan@gmail.com>
2015-08-07 14:20:53 -0700

    clean up js code, fix line termination, wrong identifier, etc. script works now, but not completely correctly

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
-<script>
 
-  var guess, answer, message, userAttempts;
-  // answer is actual number of records I own
-  answer = 380;
-  userAttempts = 0;
-  message = "Don't worry about it, how could you have known?";
-  console.log(answer);
+var guess, answer, message, userAttempts, guess1;
+// answer is actual number of records I own
+answer = 380;
+userAttempts = 0;
+message = "Don't worry about it, how could you have known?";
+console.log(answer);
 
-  	while (userAttempts < 5 && guess != answer){
-  		guess = prompt("How many vinyl records do I own?");
-  		if (guess == answer) {
-  			message = "You must be a stalker!";
-  		}
-  			// did not properly terminate lines 11 and 13 in original code
-  			// fixed next day
-  	
-    	userAttempts++;
-  	}
-
-  var recordCounter = document.getElementById('record-counter');
-    recordCounter.innerHTML = message;
-
-    // function = addToHintList() {
-    //   var hintListItem = document.createElement('li';)
-    //   var hintNode = document.createTextNode(hint1);
-    //   hintListItem.appendChild(hintNode);
-    //   document.getElementById('hint-list').appendChild(hintListItem);
+  while (userAttempts < 5 && guess != answer){
+   guess = prompt("How many vinyl records do I own?");
+    if (guess == answer) {
+      message = "You must be a stalker!";
+    }
+    userAttempts++;
   }
-  	// alert(message);
+
+var recordCounter = document.getElementById('record-counter');
+  recordCounter.innerHTML = message;
+
+  function addToGuessList() {
+   var guessListItem = document.createElement('li');
+   var guessNode = document.createTextNode(guess1);
+   guessListItem.appendChild(guessNode);
+   document.getElementById('guess-list').appendChild(guessListItem);
+  }
+  // alert(message);
   console.log(userAttempts);
 
-</script>
+
+
+
 
 
