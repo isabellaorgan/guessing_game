afa2f9d01cb80cba3a9ec8b375defea807373326
Isabella Organ <isabellaorgan@gmail.com>
2016-03-16 13:35:27 -0700

    update code style
    remove excess new lines, indentation, single quotes

--- js/main.js
+++ js/main.js
@@ -2,34 +2,27 @@
 var guess, answer, message, userAttempts, guess1;
 answer = 380;
 userAttempts = 0;
-message = "Don't worry about it, how could you have known?";
+message = 'Don\'t worry about it, how could you have known?';
 console.log(answer);
 
-  while (userAttempts < 5 && guess != answer) {
-   guess = prompt("How many vinyl records do I own?");
-   addToGuessList(guess);
-    if (guess == answer) {
-      message = "You must be a stalker!";
-    }
-    userAttempts++;
+while (userAttempts < 5 && guess != answer) {
+  guess = prompt('How many vinyl records do I own?');
+  addToGuessList(guess);
+  if (guess == answer) {
+    message = 'You must be a stalker!';
   }
+  userAttempts++;
+}
 
-  console.log(guess1);
+console.log(guess1);
 
 var recordCounter = document.getElementById('record-counter');
-  recordCounter.innerHTML = message;
+recordCounter.innerHTML = message;
 
-  function addToGuessList(gs) {
-   var guessListItem = document.createElement('li');
-   var guessNode = document.createTextNode(gs);
-   guessListItem.appendChild(guessNode);
-   document.getElementById('guesslist').appendChild(guessListItem);
-  
-  }
+function addToGuessList(gs) {
+  var guessListItem = document.createElement('li');
+  var guessNode = document.createTextNode(gs);
+  guessListItem.appendChild(guessNode);
+  document.getElementById('guesslist').appendChild(guessListItem);
 
-
-
-
-
-
-
+}
